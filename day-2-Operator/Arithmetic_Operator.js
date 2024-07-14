9bee9559eca6d21d7321db78f32ff145ffa6a4e1
Darshan Hingu <darshanhingu03@gmail.com>
2024-07-14 13:49:07 +0530

    Day 2 , activity 2 complate

---
+++ day-2-Operator/Arithmetic_Operator.js
@@ -0,0 +1,26 @@
+// task-1: write a program to add two numbers and log the result to the console.
+
+var a = 10;
+var b = 5;
+var c = a + b;
+console.log("task-1: addition - ", c);
+
+// task-2: write a program to suntract two numbers and log the result to the console.
+
+c = a - b;
+console.log("task-2: subtract - ", c);
+
+// task-3: write a program to multiple two numbers and log the result to the console.
+
+c = a * b;
+console.log("task-3: multiple - ", c);
+
+// task-4: write a program to divide two numbers and log the result to the console.
+
+c = a / b;
+console.log("task-4: divide - ", c);
+
+// task-5: write a program to find the remainder when one number is divided by another and log the result to the console.
+
+c = a % b;
+console.log("task-5: Modulo - ", c);
---
+++ day-2-Operator/Assignment_Operator.js
@@ -0,0 +1,11 @@
+// task-1: Use the += operator to add a number to a variable and log the result to the console.
+
+var a = 10;
+// var b += a; is not working
+var b = 5;
+b += a;
+console.log("task-1: ", b);
+
+// task-2: Use the -= operator to subtract a number to a variable and log the result to the console.
+b -= a;
+console.log("task-2: ", b);
