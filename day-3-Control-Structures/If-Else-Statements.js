36083280f4431aef8e1a448da0adfccb0db6920a
Darshan Hingu <darshanhingu03@gmail.com>
2024-07-15 09:50:14 +0530

    Activity 1 and 2 complate

---
+++ day-3-Control-Structures/If-Else-Statements.js
@@ -0,0 +1,22 @@
+// Activity:1 - If-Else Statements
+
+// task - 1: Write a Program to check if a number is positive, Negative, or Zero, and log the result to the console.
+
+console.log("--------- Activity 1 -----------");
+const number = 5;
+if (number > 0) {
+  console.log("task-1: Positive Number ", number);
+}
+if (number === 0) {
+  console.log("task-1: Zero ", number);
+}
+if (number < 0) {
+  console.log("task-1: Negative Number ", number);
+}
+
+// task - 2: Write a Program to check if a person is eligible to vote (age >= 18), and log the result to the console.
+
+const age = 19;
+if (age >= 18) {
+  console.log("task-2: eligible");
+}
---
+++ day-3-Control-Structures/Nested_Else_If.js
@@ -0,0 +1,15 @@
+// Activity:2 - Elseif Statements
+
+// task - 1: Write a Program to find the largest of three numbers using nested ifElse Statement
+
+const a = 10,
+  b = 30,
+  c = 100;
+
+if (a > b && a > c) {
+  console.log("task-1 a: ", a);
+} else if (b > a && b > c) {
+  console.log("task-1 b: ", b);
+} else {
+  console.log("task-1 c: ", c);
+}
