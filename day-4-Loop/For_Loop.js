ec53b6deaebcbd35d17d9d1093edd49e1c5b8149
Darshan Hingu <darshanhingu03@gmail.com>
2024-07-16 09:39:32 +0530

    activity 1 complate

---
+++ day-4-Loop/For_Loop.js
@@ -0,0 +1,17 @@
+// Activity:1 - For Loop
+
+// task-1: Write a program to print Numbers from 1 to 10 using loop.
+
+for (let i = 1; i <= 10; i++) {
+  console.log("task:1 ", i);
+}
+
+console.log("\n");
+
+// task-2: Write a program to print the multiplication table of 5 using loop.
+
+const table = 5;
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`task:2 - ${table} * ${i} = ${table * i}`);
+}
